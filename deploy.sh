132aa1db0acc186714eef696c72b3ba5145e109e
Saivickna Raveendran <saivickna.r@gmail.com>
2017-02-16 12:59:02 -0800

    (fix) Updated test scripts to pass to allow for deployment

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 echo "stopping running application"
-ssh $DEPLOY_USER@$DEPLOY_HOST 'docker stop dodsv'
-ssh $DEPLOY_USER@$DEPLOY_HOST 'docker rm dodsv'
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker stop nanongage/ngage-db'
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker rm nanongage/ngage-db'
 
 echo "pulling latest version of the code"
 ssh $DEPLOY_USER@$DEPLOY_HOST 'docker pull nanongage/ngage-db:latest'
 
 echo "starting the new version"
-ssh $DEPLOY_USER@$DEPLOY_HOST 'docker run -d --restart=always --name ngage-db -p 49016:8080 nanongage/ngage-db:latest'
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker run -d --restart=always --name ngage-db -p 49016:5000 nanongage/ngage-db:latest'
 
 echo "success!"
 
